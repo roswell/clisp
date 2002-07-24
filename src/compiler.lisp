84e0e8b6c0b0895a74ec6cf9db70e418e452c65b
Sam Steingold <sds@gnu.org>
2002-07-24 17:42:28 +0000

    1.87, 1.88 for 2.29

--- src/compiler.lisp
+++ src/compiler.lisp
@@ -2722,9 +2722,8 @@ for-value   NIL or T
 ;; returns a function name, that is composed by the package and the printname
 ;; of a given function name, a hyphen and a suffix.
 (defun symbol-suffix (funname suffix)
-  (if (and (symbolp funname) (not (and funname (symbol-package funname)))
-           (function-name-p suffix))
-    suffix
+  (if (and (symbolp funname) (not (and funname (symbol-package funname))))
+    (if (function-name-p suffix) suffix (make-symbol (princ-to-string suffix)))
     (multiple-value-bind (name pack) (get-funname-string+pack funname)
       ;; convert suffix to a string:
       (cond ((symbolp suffix) (setq suffix (symbol-name suffix)))
@@ -11243,6 +11242,7 @@ The function make-closure is required.
 
 (defmacro with-compilation-unit ((&key override) &body forms)
   `(let ((*c-top-call* (or ,override (not (boundp '*c-top-call*))))
+         (*c-listing-output* nil)
          (*c-error-output* *error-output*))
      ;; clean up from the outer `with-compilation-unit':
      ;; <http://www.lisp.org/HyperSpec/Body/mac_with-compilation-unit.html>
