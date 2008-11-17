b4d22f81dc5b32ca540c09e53716c00eed1d95eb
cvs2hg <devnull@localhost>
2008-11-17 18:47:13 +0000

    fixup commit for branch 'thread2'

--- version.sh
+++
@@ -1,8 +0,0 @@
-# Version number and release date.
-# VERSION_NUMBER should be
-#  = MAJOR.MINOR for release,
-#  = MAJOR.MINOR+ for development after MAJOR.MINOR, and
-#  = MAJOR.MINOR++ for pretest (becomes (MINOR+1) on release)
-# RELEASE_DATE should be the date when VERSION_NUMBER was modified.
-VERSION_NUMBER=2.47+
-RELEASE_DATE=2008-10-24         # in "date +%F" format
